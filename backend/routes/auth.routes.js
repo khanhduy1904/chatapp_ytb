8f56acf63d9db3649e99070aa5b46c5279d14df6
khanhduy1904 <khanhduyphamz19@gmail.com>
2024-04-19 23:40:30 +0700

    1st part - backend

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  login,
+  logout,
+  signup,
+  testRoute,
+} from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+router.post("/test", testRoute);
+
+export default router;
