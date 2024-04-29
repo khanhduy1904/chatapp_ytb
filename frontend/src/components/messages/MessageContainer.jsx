3992198f62c4d01b00a4f13ae52777d906faeba0
khanhduy1904 <khanhduyphamz19@gmail.com>
2024-04-30 00:28:42 +0700

    Socket Implement

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,30 @@
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import { TiMessages } from "react-icons/ti";
+import useConversation from "../../zustand/useConversation";
+import { useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    // clean up fin
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[650px]  flex flex-col bg-white">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           {/* Header */}
           <div className="bg-gray-100 border-r  border-gray-600 px-4 py-2 h-15">
             <span className="label-tex text-1xl text-black">To:</span>{" "}
-            <span className="text-gray-900 font-bold text-2xl ">John doe</span>
+            <span className="text-gray-900 font-bold text-2xl ">
+              {selectedConversation.username}
+            </span>
           </div>
 
           <Messages />
@@ -27,11 +38,11 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
-  //   const { authUser } = useAuthContext();
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full bg-blend-saturation">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-500 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 Khanh Duy ❄</p>
+        <p>Welcome 👋 {authUser.username} ❄</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,40 @@
-import Message from "./Message.jsx";
+import { TiMessages } from "react-icons/ti";
+import useGetMessages from "../../hooks/useGetMessages.js";
+import MessageSkeleton from "../../skeletons/MessageSkeletons.jsx";
+import Message from "./Message";
+import { useEffect, useRef } from "react";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  useListenMessages();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
   return (
     <div className="px-4 flex-1 overflow-auto bg-white">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      {!loading && messages.length === 0 && (
+        <div className="flex items-center justify-center w-full h-full bg-blend-saturation">
+          <div className="px-4 text-center sm:text-lg md:text-xl text-gray-500 font-semibold flex flex-col items-center gap-2">
+            <p>Send a message to start the conversation</p>
+            <TiMessages className="text-3xl md:text-6xl text-center" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
